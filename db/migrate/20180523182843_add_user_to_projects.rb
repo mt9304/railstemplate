2ff4bcf6404c0a9f5831dfde6a88589d68720687
Max <mt9304@gmail.com>
2018-05-25 12:05:50 -0700

    Still looking into permissions issue.

--- db/migrate/20180523182843_add_user_to_projects.rb
+++ db/migrate/20180523182843_add_user_to_projects.rb
@@ -1,5 +1,5 @@
 class AddUserToProjects < ActiveRecord::Migration[5.2]
   def change
-    add_reference :projects, :user, foreign_key: true
+    add_reference :projects, :user, index: true, foreign_key: true
   end
 end
